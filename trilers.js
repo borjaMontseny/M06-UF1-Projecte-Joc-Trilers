2d0180de0549c42c70409e8f2e70c7af2cbd7947
Borja Montseny <borja.montseny@gmail.com>
2023-03-15 12:46:35 +0100

    .

--- trilers.js
+++ trilers.js
@@ -1,6 +1,6 @@
 // crearem el taulell amb els trilers seqons la dificultad i multiplicadors des d'una matriu
 
-// les files son la dificultat, columna 0 representen els trilers (0 , 1 , 2) | la columna 1 els multiplicadors
+// les files son la dificultat, columna 0 representen els trilers | la columna 1 els multiplicadors
 var trilersIMultiplicadors = [[3, 2], [5, 5], [7, 10]];
 
 var nomUsuari;
@@ -10,6 +10,12 @@ var dificultatEscollida;
 do {
 
     nomUsuari = prompt('Benvingut al casino IBC\n\nIntrodueix el teu nom:', 'Player');
+
+    while (nomUsuari.length == 0) {
+
+        nomUsuari = prompt('Siusplau\nIntrodueix un nom vàlid:', 'Player');
+
+    }
 
     dificultatEscollida = parseInt(prompt('Escolleix la dificultat:\n\n0. Fàcil         [ 3 Trilers & Beneficis X 2 ]\n\n1. Intermig  [ 5 Trilers & Beneficis X 5 ]\n\n2. Difícil      [ 7 Trilers & Beneficis X 10 ]\n\n', 0));
 
@@ -21,7 +27,7 @@ do {
 document.write('<div id="board">');
 document.write('<div id="board">');
 
-// imprimim cubilets segons la dificultat (qtat de cubilets guardada al array a les columnes 0)
+// imprimim cubilets segons la dificultat (qtat de cubilets guardada al array a les columnes 0 segons dificultat (fila))
 for (let index = 0; index < trilersIMultiplicadors[dificultatEscollida][0]; index++) {
 
     document.write('<div class="beaker" id="' + index + '">' + index + '</div>');
@@ -53,8 +59,10 @@ let numUsuari;
 
 function jocTrilers() {
 
+    // demanem els diners a apostar
     dinersApostats = Number(prompt('Quants diners vols apostar?'));
 
+    // comprovant de que ho introïm una quantitat vàlida
     while (dinersApostats <= 0) {
 
         dinersApostats = Number(prompt('Introdueix una quantitat vàlida.', 1));
@@ -63,14 +71,15 @@ function jocTrilers() {
 
     numRandom = crearAleatori(1, (trilersIMultiplicadors[dificultatEscollida][0]) - 1);
 
-    // probes pel desenvolupador
+    // probes per al desenvolupador, per saber acertar sempre (o no)
     console.log('La boleta està al cubilet ' + numRandom);
 
+    // bucle per introduir num dins del rang de cubilets generats
     numUsuari = parseInt(prompt('En que cubilet creus que està la boleta?'));
 
     while ((numUsuari < 0 && numUsuari > trilersIMultiplicadors[dificultatEscollida][0]) || isNaN(numUsuari)) {
 
-        numUsuari = parseInt(prompt("Siusplau\nDiga'm un cubilet entre 0 i " + trilersIMultiplicadors[dificultatEscollida][0] + '!'));
+        numUsuari = parseInt(prompt("Siusplau\nDiga'm un cubilet entre 0 i " + (trilersIMultiplicadors[dificultatEscollida][0] - 1) + '!'));
 
     }
 
@@ -86,7 +95,7 @@ function jocTrilers() {
 
         saldoRestant = saldoRestant - dinersApostats;
 
-        alert('Has perdut!\nLa boleta estava al cubilet ' + numRandom + ' i apostat al ' + numUsuari);
+        alert('Quina pena ' + nomUsuari + ' has perdut!\nLa boleta estava al cubilet ' + numRandom + ' i has apostat al ' + numUsuari);
 
     }
 
